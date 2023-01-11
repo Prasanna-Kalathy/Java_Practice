e7d3f07edda88db1d67d0a206c48807f49c5a42d
Prasanna-Kalathy <prasannakalathy@gmail.com>
2023-01-11 11:13:32 +0530

    Special Number

--- Numbers/src/com/pk/numbers/SpecialNumber.java
+++ Numbers/src/com/pk/numbers/SpecialNumber.java
@@ -7,11 +7,18 @@ public class SpecialNumber {
 	public static void main(String[] args) {
 		Scanner In = new Scanner(System.in);
 		System.out.println("Enter the Number");
-		int num = In.nextInt();
-		
-		while(num!=0) {
-			
+		int dig, sum = 0, prod = 1, num = In.nextInt();
+		int temp = num;
+		while (num != 0) {
+			dig = num % 10;
+			sum += dig;
+			prod *= dig;
+			num /= 10;
 		}
+		if (temp == (sum + prod)) {
+			System.out.println(temp + " is a Special Number");
+		} else
+			System.out.println(temp + " is Not a Special Number");
 	}
 
 }
